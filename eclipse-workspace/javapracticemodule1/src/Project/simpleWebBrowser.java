5c7c3f44fdb1d0e21f93b87dafc8a969d3d22b2c
flowersofmalnad <flowersofmalnadregion@github.com>
2020-12-13 22:46:52 +0530

    My first project in Java- a simple web browser

---
+++ eclipse-workspace/javapracticemodule1/src/Project/simpleWebBrowser.java
@@ -0,0 +1,59 @@
+package Project;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class simpleWebBrowser extends JFrame{
+	
+	private JTextField addressBar;
+	private JEditorPane display;
+	
+	//constructor
+	public simpleWebBrowser() {
+		super("My Simple Browser");
+		
+		addressBar = new JTextField("Enter an Url!");
+		addressBar.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent event) {
+						loadCrap(event.getActionCommand());
+							
+					}
+				}
+		);
+		add(addressBar,BorderLayout.NORTH);
+		
+		display= new JEditorPane();
+		display.setEditable(false);
+		display.addHyperlinkListener(
+				new HyperlinkListener() {
+					public void hyperlinkUpdate(HyperlinkEvent event) {
+						if(event.getEventType()==HyperlinkEvent.EventType.ACTIVATED) {
+							loadCrap(event.getURL().toString());
+							
+						}
+					}
+				}
+				
+	   );
+		add(new JScrollPane(display),BorderLayout.CENTER);
+		setSize(500,300);
+		setVisible(true);
+		
+		
+		
+	}
+
+	//load crap to display on the screen
+	private void loadCrap(String userText) {
+		try {
+			//this  code gets executed for right Url
+			display.setPage(userText);
+			addressBar.setText(userText);
+		}catch(Exception e) {
+			System.out.println("Check the URL!");
+		}
+	}
+
+}
---
+++ eclipse-workspace/javapracticemodule1/src/Project/simpleWebBrowserMain.java
@@ -0,0 +1,11 @@
+package Project;
+import javax.swing.JFrame;
+
+public class simpleWebBrowserMain {
+	public static void main(String[] args) {
+		simpleWebBrowser dude=new simpleWebBrowser();
+		dude.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+}
